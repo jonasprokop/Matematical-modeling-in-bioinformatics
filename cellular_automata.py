2b32f7f39e2e8f30740d092a96875e7a14e7de48
Jonas Prokop <jonas.prokop22@gmail.com>
2024-11-12 14:01:26 +0100

    cellular automata numpy impl

--- cellular_automata.py
+++ cellular_automata.py
@@ -1,7 +1,15 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
+
+
+# Rule 90
 value_grid = {0:0, 1:1, 2:0, 3:1, 4:1, 5:0, 6:1, 7:0}
+
+
+# dict implementation
+
 entry_grid = {i: random.randint(0, 1) for i in range(500)}
 
 def convert_to_binary_compare(input_list, value_grid):
@@ -14,10 +22,13 @@ def convert_to_binary_compare(input_list, value_grid):
 
     return result
 
-def evolve_grid(n, value_grid, entry_grid):
+
+# evolve grid from state t to state t+1
+def evolve_grid(value_grid, entry_grid):
     new_grid = {}
     val_1 = False
     val_2 = False
+    n = len(entry_grid)
     for key, value in entry_grid.items():
         val_1 = entry_grid[(key - 2) % n]  
         val_2 = entry_grid[(key + 2) % n]  
@@ -28,6 +39,7 @@ def evolve_grid(n, value_grid, entry_grid):
 
     return new_grid
 
+# iterate evolution n times
 def evolve_grid_n_times(epochs, value_grid, entry_grid):
     results = []
     for epoch in range(epochs):
@@ -35,8 +47,10 @@ def evolve_grid_n_times(epochs, value_grid, entry_grid):
         results.append(list(entry_grid.values()))
     return results
 
+
 # Running the evolution for 500 iterations
 results = evolve_grid_n_times(500, value_grid, entry_grid)
+
 
 # Visualize the evolution of the grid
 plt.figure(figsize=(15, 10))
@@ -46,3 +60,42 @@ plt.xlabel("Cell Index")
 plt.ylabel("Epoch")
 plt.title("Evolution of Cellular Automaton")
 plt.show()
+
+
+
+# # numpy implemntation
+
+# entry_grid = np.array([random.randint(0, 1) for _ in range(500)])
+
+## evolve grid from state t to state t+1
+# def evolve_grid_numpy(value_grid, entry_grid):
+#     left_neighbors = np.roll(entry_grid, -2)
+#     right_neighbors = np.roll(entry_grid, 2)
+
+#     new_grid = np.array([
+#         convert_to_binary_compare([left_neighbors[i], entry_grid[i], right_neighbors[i]], value_grid)
+#         for i in range(len(entry_grid))
+#     ])
+
+#     return new_grid
+
+## iterate evolution n times
+# def evolve_grid_n_times_numpy(epochs, value_grid, entry_grid):
+#     results = [entry_grid]  
+#     for _ in range(epochs):
+#         entry_grid = evolve_grid_numpy(value_grid, entry_grid)
+#         results.append(entry_grid)  
+#     return np.array(results) 
+
+
+# results = evolve_grid_n_times_numpy(500, value_grid, entry_grid)
+
+
+# # Visualize the evolution of the grid
+# plt.figure(figsize=(15, 10))
+# plt.imshow(results, cmap='binary', interpolation='nearest', aspect='auto')
+# plt.colorbar(label="State (0 or 1)")
+# plt.xlabel("Cell Index")
+# plt.ylabel("Epoch")
+# plt.title("Evolution of Cellular Automaton")
+# plt.show()
